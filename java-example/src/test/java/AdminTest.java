0068ba78e3fe979c44f40a64662f507979adb4cc
Анастасия <ms.zloy@list.ru>
2018-03-21 21:59:42 +0300

    Создан тест логина в админку

---
+++ java-example/src/test/java/AdminTest.java
@@ -0,0 +1,50 @@
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class AdminTest {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void AdminTest() {
+        wd.get("http://localhost:8080/litecart/admin/login.php?redirect_url=%2Flitecart%2Fadmin%2F");
+        wd.findElement(By.name("username")).click();
+        wd.findElement(By.name("username")).clear();
+        wd.findElement(By.name("username")).sendKeys("admin");
+        wd.findElement(By.name("password")).click();
+        wd.findElement(By.name("password")).clear();
+        wd.findElement(By.name("password")).sendKeys("admin");
+        wd.findElement(By.name("login")).click();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
