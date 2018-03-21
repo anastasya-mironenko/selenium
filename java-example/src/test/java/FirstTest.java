7190cccf4f688139264df6e5037a36d940e7df1e
Анастасия <ms.zloy@list.ru>
2018-03-21 20:24:35 +0300

    Создан первый тест

---
+++ java-example/src/test/java/FirstTest.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class FirstTest {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void FirstTest() {
+        wd.get("https://mail.ru/");
+        wd.findElement(By.id("q")).click();
+        wd.findElement(By.id("q")).clear();
+        wd.findElement(By.id("q")).sendKeys("selenium");
+        wd.findElement(By.id("search:submit")).click();
+        wd.findElement(By.linkText("Selenium - Web Browser Automation")).click();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
